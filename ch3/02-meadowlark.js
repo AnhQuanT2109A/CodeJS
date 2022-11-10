a34017dfc565c976f7691c4e0cde3abcfc23002d
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-11-10 14:40:35 +0700

    ok

---
+++ ch3/02-meadowlark.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
