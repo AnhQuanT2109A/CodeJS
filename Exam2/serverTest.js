4699e382fc02714cc53e32ef6d1524bee24e7898
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-11-15 13:20:09 +0700

    CH6

--- Exam2/serverTest.js
+++ Exam2/serverTest.js
@@ -13,19 +13,20 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => res.render('about'))
-   
-// custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+app.get('/about', (req, res) => {
+    const randomFortune =
+        fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
 })
-//    custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+
+
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port};` + `pree crtl + C to terminate`
 ))
--- ch3/00-meadowlark.js
+++ ch3/00-meadowlark.js
@@ -3,17 +3,17 @@ const app = express()
 const port = process.env.PORT || 3000
 // custom 404 page
 app.use((req, res) => {
- res.type('text/plain')
- res.status(404)
- res.send('404 - Not Found')
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
 })
 // custom 500 page
 app.use((err, req, res, next) => {
- console.error(err.message)
- res.type('text/plain')
- res.status(500)
- res.send('500 - Server Error')
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
 })
 app.listen(port, () => console.log(
- `Express started on http://localhost:${port}; ` +
- `press Ctrl-C to terminate.`))
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
