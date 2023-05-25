763df62f2cebffbfb36c6ab13929946bd5a7f17c
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2023-05-25 09:22:43 +0700

    submit

---
+++ Crud/src/main/java/mvcCrud/ProductServlet.java
@@ -0,0 +1,116 @@
+package mvcCrud;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/products")
+public class ProductServlet extends HttpServlet {
+    private List<Product> productList;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        productList = new ArrayList<>();
+        productList.add(new Product(1, "Computer", 32.20));
+        productList.add(new Product(2, "Apple Watch", 99.50));
+        productList.add(new Product(3, "Iphone 14", 49.9));
+        productList.add(new Product(4, "Ipad", 300.00));
+        productList.add(new Product(5, "SamSung", 127.60));
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "list";
+        }
+
+        switch (action) {
+            case "new":
+                showNewForm(request, response);
+                break;
+            case "create":
+                createProduct(request, response);
+                break;
+            case "edit":
+                showEditForm(request, response);
+                break;
+            case "update":
+                updateProduct(request, response);
+                break;
+            case "delete":
+                deleteProduct(request, response);
+                break;
+            default:
+                listProducts(request, response);
+                break;
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private void listProducts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("productList", productList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product-list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product-form.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void createProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+        int id = productList.size() + 1;
+
+        Product newProduct = new Product(id, name, price);
+        productList.add(newProduct);
+
+        response.sendRedirect("products");
+    }
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product product = getProductById(id);
+
+        request.setAttribute("product", product);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product-form.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void updateProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+
+        Product product = getProductById(id);
+        product.setName(name);
+        product.setPrice(price);
+
+        response.sendRedirect("products");
+    }
+    private void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product product = getProductById(id);
+        productList.remove(product);
+
+        response.sendRedirect("products");
+
+    }
+    private Product getProductById(int id) {
+        for (Product product : productList) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
